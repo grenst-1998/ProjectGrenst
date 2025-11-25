749b12402b7773373272abb93aa7f3165633a3ac
grenst-1998 <grenst.mel@gmail.com>
2025-11-25 18:04:18 -0500

    Primercommit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'principal',
+    pathMatch: 'full',
+  },
+  {
+    path: 'principal',
+    loadComponent: () => import('./paginas/principal/principal.page').then( m => m.PrincipalPage)
+  },
+
+];
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar,
+    IonTitle, IonContent, IonGrid, IonRow, IonCol],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
+
+})
+export class HomePage {
+  constructor() {}
+
+  message: string | null = null;
+
+  showMessage() {
+    this.message = '¡Felicidades! Has creado tu primera app con ionic';
+  }
+
+}
